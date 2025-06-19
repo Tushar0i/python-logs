e760ff1cfda597b642db2160a7edec68a57d2625
Tushar0i <tushar2006thakur@gmail.com>
2025-06-20 02:02:58 +0530

    Data Types

---
+++ Fundamentals of Python/02.py
@@ -0,0 +1,26 @@
+# Data type
+
+name = "tushar"
+print(type(name))
+print(type(name) == str)
+
+print(isinstance(name,str))
+
+age = 19
+weight = 75.5
+print(isinstance(age,int))
+print(type(weight))
+# pyhton automatically know the data type of the variabele that we are using
+
+num = "20"
+number = int(num)
+# we can convert data type this called casing
+print(type(number))
+
+#  complex for complex number
+#  bool for booleans 
+#  list for lists
+#  tuple for tuples
+#  range for ranges
+#  dict for dictionaries
+#  set for sets
