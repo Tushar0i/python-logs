ebc6a47c87300eeb54d427510fbccdce82197e83
Tushar0i <tushar2006thakur@gmail.com>
2025-06-19 22:45:38 +0530

    Variables, Expression,Statement and Comments in Python

---
+++ Fundamentals of Python/01.py
@@ -0,0 +1,13 @@
+#Variables, Expression,Statement and Comments
+
+
+#this is a commanted statement weich is ignored in exicution
+
+name  = "something "; print(name)
+
+# ';' works as a line break 
+# Indentation is important in python 
+
+x,y,z = 1,2,3
+
+
