7d8690e557194b36b998e93636b32979fc64a9f3
Tushar0i <tushar2006thakur@gmail.com>
2025-06-21 20:05:43 +0530

    Number data type

---
+++ Fundamentals of Python/06.py
@@ -0,0 +1,18 @@
+# Number Data type
+
+# Integers
+num1 =  2 # discrete numbers 
+print(type(num1))
+
+# Floating point number
+num2 = 2.0 # contains decimal value
+print(type(num2))
+
+# Complex numbers
+
+compnum1 = 2+4j
+compnum2 = complex(4,9)
+print(compnum1.real,compnum1.imag)
+print(compnum2.real,compnum2.imag)
+
+print(type(compnum1))
