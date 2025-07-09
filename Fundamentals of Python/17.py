3bef9574db364ac17e52e8c6245f4d543bcfdfb8
Tushar0i <tushar2006thakur@gmail.com>
2025-07-10 04:43:19 +0530

    Modules in detail

---
+++ Fundamentals of Python/17.py
@@ -0,0 +1,23 @@
+# Modules
+
+# from lib import dog 
+from lib.dog import bark 
+from math import sqrt as square_root
+
+bark()
+num = square_root(99)
+print(num)
+
+# python have a standerd lib 
+# some of the module are ->
+# math for math utilities
+# re for regular expressions
+# json to work with JSON
+# datetime to work with dates
+# sqlite3 to use SQLite
+# os for Operating System utilities
+# random for random number generation
+# statistics for statistics utilities
+# requests to perform HTTP network requests
+# http to create HTTP servers
+# urllib to manage URLS
